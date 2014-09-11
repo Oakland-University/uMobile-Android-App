d31d2c354d5238b1f7e149072e009c2b9cef2e36
swiggins83 <stevenwiggins83@gmail.com>
2014-09-11 14:20:00 -0400

    Create Login UI template

--- app/src/main/java/org/apereo/activities/LoginActivity.java
+++ app/src/main/java/org/apereo/activities/LoginActivity.java
@@ -1,14 +1,9 @@
 package org.apereo.activities;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.util.Log;
 import android.webkit.CookieManager;
-import android.webkit.WebSettings;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -18,9 +13,6 @@ import org.androidannotations.annotations.Bean;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.Extra;
 import org.androidannotations.annotations.ViewById;
-import org.androidannotations.annotations.rest.Rest;
-import org.apache.commons.lang.StringUtils;
-import org.apereo.App;
 import org.apereo.R;
 import org.apereo.constants.AppConstants;
 import org.apereo.deserializers.LayoutDeserializer;
@@ -33,7 +25,7 @@ import org.apereo.utils.Logger;
 /**
  * Created by schneis on 8/28/14.
  */
-@EActivity(R.layout.portlet_webview)
+@EActivity(R.layout.login_page)
 public class LoginActivity extends BaseActivity {
 
     private static final String TAG = LoginActivity.class.getName();
@@ -58,36 +50,7 @@ public class LoginActivity extends BaseActivity {
 
     @AfterViews
     void initiailize() {
-        WebSettings webSettings = webView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-        webView.setWebViewClient(new WebViewClient() {
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                return super.shouldOverrideUrlLoading(view, url);
-            }
-
-            @Override
-            public void onPageStarted(WebView view, String url, Bitmap favicon) {
-                if(url.equalsIgnoreCase(getString(R.string.home_page))) {
-                    App.setIsAuth(true);
-                    getLoggedInFeed();
-                }
-                Logger.d(TAG, "starting " + url);
-                super.onPageStarted(view, url, favicon);
-
-            }
-
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                if (StringUtils.equalsIgnoreCase(url, getString(R.string.logout_url))) {
-                    App.setIsAuth(false);
-                    restApi.setCookie("");
-                    getFeed();
-                }
-                super.onPageFinished(view, url);
-            }
-        });
-        webView.loadUrl(url);
+        // TODO: Make UI functional
     }
 
     private void getLoggedInFeed() {
